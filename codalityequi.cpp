8ed23d8170de859b7e8e43231fec5cf5fa25243e
jobediah <douglas41@hotmail.com>
2014-10-22 15:10:47 -0600

    Update codality test

--- codalityequi.cpp
+++ codalityequi.cpp
@@ -11,24 +11,35 @@ using namespace std;
 
 
 void solution(vector<long long int> &A, long long int n) {
-    long long int sum1, sum2, sum3;
+    long long int sum = 0;
+    long long int Lsum = 0;
+    long long int Rsum = 0;
     int counter = 0;
+    int n = A.size();
+    
+    if(n <= 1){
+        cout<<0<<endl;
+        break;
+    }
+    
     for(int j = 0; j < n; j++){
-        sum1 += A[j];
+        sum += A[j];
     }
     
     for(int i = 0; i < n; i++){
-        sum2 += A[i];
-        sum3 = sum1 - sum2 - (long long int)A[i+1];
-        if(sum2 == sum3){
-            cout<<(i+1)<<endl;
+        Rsum = sum - Lsum - (long long int)A[i];
+        if(Lsum == Rsum){
+            cout<<i<<endl;
             counter++;
         }
+        
+        Lsum += A[i];
     }
     
     if(counter == 0){
         cout<<-1<<endl;
     }
+    return 0;
 }
     
     int main() {
@@ -40,6 +51,7 @@ void solution(vector<long long int> &A, long long int n) {
             a.push_back(integer);
         }
         
-        solution(a, n);
+        solution(a, n)
+        
         return 0;
     }
